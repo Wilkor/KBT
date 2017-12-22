05ab100c6c8613f38d0020dc89eb16cea1ef6ad4
Unknown <keillarl@hotmail.com>
2017-12-22 12:27:03 -0200

    Mudança da classe de constantes para uso de melhor padrão.

--- src/main/java/constant/ExtratorConstants.java
+++ src/main/java/constant/ExtratorConstants.java
@@ -4,10 +4,10 @@ package constant;
  * @author Keila Lacerda
  *
  */
-public interface ExtratorConstants {
+public final class ExtratorConstants {
 
-	public String SHEET_NAME_SINONIMOS = "Sinônimos";
-	public int CELL_INDEX_ENTITY = 1;
-	public int SINONIMOS_CELL_BEGIN = 2;
-	public int SINONIMOS_CELL_END = 6;
+	public static final String SHEET_NAME_SINONIMOS = "Sinônimos";
+	public static final int CELL_INDEX_ENTITY = 1;
+	public static final int SINONIMOS_CELL_BEGIN = 2;
+	public static final int SINONIMOS_CELL_END = 6;
 }
