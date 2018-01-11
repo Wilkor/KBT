da8396d6ead897bc896f73cc9e3ec31eeb27f888
Guilherme Miari <gpmiari@gmail.com>
2018-01-11 18:43:06 -0200

    solving bug on validate entity

--- src/main/java/validator/EntityValidator.java
+++ src/main/java/validator/EntityValidator.java
@@ -3,11 +3,13 @@ package validator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.limeprotocol.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.Intention;
 import model.KnowledgeBase;
 import net.take.iris.messaging.resources.artificialIntelligence.Entity;
 import net.take.iris.messaging.resources.artificialIntelligence.Entity.EntityValues;
@@ -25,18 +27,24 @@ public class EntityValidator {
 			List<Entity> entityInCat = new ArrayList<>();
 
 			List<Entity> entityWithoutValues = new ArrayList<>();
-			
+
 			if (entities.size() > 0) {
 
 				for (Entity en : entities) {
+
+					Predicate<Entity> p = e -> e.getName().equals(en.getName());
+
 					for (EntityValues ev : Arrays.asList(en.getValues())) {
 						if (ev.getName().equals(en.getName())) {
 							entityInCat.add(en);
 						}
 
 						if (StringUtils.isNullOrEmpty(ev.getName())) {
-							LOGGER.info("Entity (" + en.getName() + ") value is empty  ", en);
-							entityWithoutValues.add(en);
+							
+							if (entityWithoutValues.stream().noneMatch(p)) {
+								entityWithoutValues.add(en);
+								LOGGER.info("Entity (" + en.getName() + ") value is empty  ", en);
+							}
 						}
 					}
 
@@ -50,7 +58,7 @@ public class EntityValidator {
 				LOGGER.info("Entities equals 0");
 			}
 
-			return !(entityInCat.size() > 0 && entityWithoutValues.size() > 0);
+			return !(entityInCat.size() > 0 || entityWithoutValues.size() > 0);
 
 		} catch (Exception e) {
 			LOGGER.error("Error on validate", e.getMessage(), e);
